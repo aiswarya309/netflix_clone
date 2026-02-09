448b871da68767c9dcc1abf48891d6666fec2d7c
Aiswarya <aishuzz309@gmail.com>
2026-02-09 12:45:55 +0530

    Footer changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './Footer.css'
+import facebook_icon from "../../assets/facebook_icon.jpg"
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <div className="footer-icons">
+        <img src={facebook_icon} alt="Facebook" />
+        <img src={facebook_icon} alt="Facebook" />
+        <img src={facebook_icon} alt="Facebook" />
+        <img src={facebook_icon} alt="Facebook" />
+      </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Center</li>
+        <li>Gift Cards</li>
+        <li>Media Center</li>
+        <li>Investor Relations</li>
+        <li>Jobs </li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notice</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text'>&copy; 1997 - 2023 Netflix, Inc.</p>
+    </div>
   )
 }
 
